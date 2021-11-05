b3a8b0ca1d528b0df6393c6c049ae70cfe93fdc8
thebartolomew <93366993+thebartolomew@users.noreply.github.com>
2021-11-05 13:30:23 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <!DOCTYPE HTML>
-<head>
-<title> My profile </title>
-</head>
+ <head>
+    <title> My profile </title>
+  <link rel="icon" type="image/x-icon" href="https://i.pinimg.com/originals/5f/73/2a/5f732a1dcd9d5b994dd8a6792af5ec7c.png">
+  <link rel="stylesheet" href="style.css"> 
+ </head>
 <body>
- <style>
-body {
-  background-image: url('https://kartinkin.net/uploads/posts/2021-07/1626700951_6-kartinkin-com-p-anime-oboi-tyomnie-vertikalnie-anime-krasi-6.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: 100% 100%;
-}
-</style>
-<h1>Profile</h1>
-<p>Temirlan</p>
-<a href="vk.com/lunat1kgod" style="color:white">My VK profile</a>
+    <style>
+    body {
+    background-image: url('https://kartinkin.net/uploads/posts/2021-07/1626700951_6-kartinkin-com-p-anime-oboi-tyomnie-vertikalnie-anime-krasi-6.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100% 100%;
+    }
+    h1{
+    text-align:center;
+ 
+    }
+    </style>
+  <h1>Profile</h1>
+    
+    <p>my name is Temirlan</p>
+  
+    <a href="https://vk.com/lunat1kgod" target="_blank">My VK profile</a>
+    <iframe width="420" height="315"
+    src="https://www.youtube.com/embed/9Ykz2-CPsjk">
+    </iframe> 
+
+  <p>Photo:</p>
+  <img src="https://i.pinimg.com/originals/34/73/89/3473894afae0a961ff2b3e06772c71ef.jpg" width="150" height="150">
+
 </body>
 </html>
