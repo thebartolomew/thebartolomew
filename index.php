de6699020a1ba7ebb8603f47b0a38c228908e900
thebartolomew <93366993+thebartolomew@users.noreply.github.com>
2021-11-12 12:45:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ body{ background-image: url('background.jpg');
 .welcome-text{font-size:1.75rem}
 .near-text{font-size:1rem}
 h1{
-font-size:45px;
+font-size:15px;
 }
 .fadeIn{-webkit-animation-name:fadeIn;animation-name:fadeIn;-webkit-animation-duration:1s;animation-duration:1s;-webkit-animation-fill-mode:both;animation-fill-mode:both}  
 }
